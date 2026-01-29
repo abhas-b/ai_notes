1974ecbfcf87edc69a0ff9d8271e07d836e1d788
abhasb7 <abhasb7@gmail.com>
2026-01-29 11:20:18 +0530

    Output formatting and parsing added using PydanticOutputParser

--- 3. react_search_agent/main.py
+++ 3. react_search_agent/main.py
@@ -5,24 +5,40 @@ load_dotenv(dotenv_path="../.env")
 from langchain_classic import hub
 from langchain_classic.agents import AgentExecutor
 from langchain_classic.agents.react.agent import create_react_agent
+# The following are used for generating structured output
+from langchain_core.output_parsers.pydantic import PydanticOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
 
+from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
+from schemas import AgentResponse
 
 tools = [TavilySearch()]
-llm = ChatOpenAI(model='gpt-4o-mini')
+llm = ChatOpenAI(model="gpt-4o-mini")
 react_prompt = hub.pull("hwchase17/react")
-agent = create_react_agent(llm=llm,tools=tools,prompt=react_prompt,)
+output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+react_prompt_with_format_instructions = PromptTemplate(
+    template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
+    input_variables=["input", "tools", "tool_names", "agent_scratchpad"],
+).partial(format_instructions=output_parser.get_format_instructions())
+
+
+agent = create_react_agent(
+    llm=llm,
+    tools=tools,
+    prompt=react_prompt_with_format_instructions,
+)
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 chain = agent_executor
+
 
 def main():
     print("Hello from 3-react-search-agent! - All imports done")
     query = """search for 3 job postings for an ai engineer using langchain in bangalore or bengaluru on linkedin and list their details."""
-    result = chain.invoke(
-        input={"input":query}
-    )
-    print(result)
+    result = chain.invoke(input={"input": query})
+    print(output_parser.parse(result["output"]))
 
 
 if __name__ == "__main__":
