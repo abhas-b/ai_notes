3f9c7d5fe2fb17be11633e208bb93f5c06dcd51f
abhasb7 <abhasb7@gmail.com>
2026-01-29 11:37:14 +0530

    Output parsing added via LCEL

--- 3. react_search_agent/main.py
+++ 3. react_search_agent/main.py
@@ -18,7 +18,9 @@ from schemas import AgentResponse
 tools = [TavilySearch()]
 llm = ChatOpenAI(model="gpt-4o-mini")
 react_prompt = hub.pull("hwchase17/react")
+
 output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+
 react_prompt_with_format_instructions = PromptTemplate(
     template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
     input_variables=["input", "tools", "tool_names", "agent_scratchpad"],
@@ -30,15 +32,18 @@ agent = create_react_agent(
     tools=tools,
     prompt=react_prompt_with_format_instructions,
 )
+
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
-chain = agent_executor
+extract_output = RunnableLambda(lambda x: x["output"])
+parse_output = RunnableLambda(lambda x: output_parser.parse(x))
+chain = agent_executor | extract_output | parse_output
 
 
 def main():
     print("Hello from 3-react-search-agent! - All imports done")
     query = """search for 3 job postings for an ai engineer using langchain in bangalore or bengaluru on linkedin and list their details."""
     result = chain.invoke(input={"input": query})
-    print(output_parser.parse(result["output"]))
+    print(result)
 
 
 if __name__ == "__main__":
